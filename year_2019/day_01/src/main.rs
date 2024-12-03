134f61cdb554401fb317fae1ded6682c53fa0cbc
Grazen0 <josedanielgrayson@proton.me>
2024-12-03 18:17:12 -0500

    feat: 2019 day 1

---
+++ year_2019/day_01/src/main.rs
@@ -0,0 +1,40 @@
+use aoc_lib::{PuzzleDate, PuzzleSolution, SolutionResult};
+
+struct Day1;
+
+fn actual_fuel(mut mass: i32) -> i32 {
+    let mut sum = 0;
+
+    loop {
+        mass = (mass / 3) - 2;
+
+        if mass > 0 {
+            sum += mass;
+        } else {
+            break;
+        }
+    }
+
+    sum
+}
+
+impl PuzzleSolution for Day1 {
+    type Input = Vec<i32>;
+    type Output = i32;
+
+    fn parse_input(raw_input: String) -> Self::Input {
+        raw_input.lines().map(|l| l.parse().unwrap()).collect()
+    }
+
+    fn part_1(input: &Self::Input) -> SolutionResult<Self::Output> {
+        Ok(input.iter().map(|n| (n / 3) - 2).sum())
+    }
+
+    fn part_2(input: &Self::Input) -> SolutionResult<Self::Output> {
+        Ok(input.iter().map(|n| actual_fuel(*n)).sum())
+    }
+}
+
+fn main() {
+    aoc_lib::cli::<Day1>(PuzzleDate::new(2019, 1));
+}
