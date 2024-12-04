10eb9ae87739b563a4595610b03ac3254da81de4
Grazen0 <josedanielgrayson@proton.me>
2024-12-03 22:59:05 -0500

    feat: 2019 day 4

---
+++ year_2019/day_04/src/main.rs
@@ -0,0 +1,67 @@
+use std::ops::RangeInclusive;
+
+use aoc_lib::{PuzzleDate, PuzzleSolution, SolutionResult};
+
+fn digits(mut n: u32) -> Vec<u32> {
+    let mut digits = Vec::new();
+
+    while n != 0 {
+        digits.insert(0, n % 10);
+        n /= 10;
+    }
+
+    digits
+}
+
+struct Day4;
+
+impl PuzzleSolution for Day4 {
+    type Input = RangeInclusive<u32>;
+    type Output = usize;
+
+    fn parse_input(raw_input: String) -> Self::Input {
+        let (min, max) = raw_input.trim().split_once("-").unwrap();
+        min.parse().unwrap()..=max.parse().unwrap()
+    }
+
+    fn part_1(range: &Self::Input) -> SolutionResult<Self::Output> {
+        let count = range
+            .clone()
+            .into_iter()
+            .filter(|passwd| {
+                let windows = digits(*passwd)
+                    .windows(2)
+                    .map(|w| (w[0], w[1]))
+                    .collect::<Vec<_>>();
+
+                windows.iter().any(|(a, b)| a == b) && windows.iter().all(|(a, b)| a <= b)
+            })
+            .count();
+
+        Ok(count)
+    }
+    fn part_2(range: &Self::Input) -> SolutionResult<Self::Output> {
+        let count = range
+            .clone()
+            .into_iter()
+            .filter(|passwd| {
+                let windows = digits(*passwd)
+                    .windows(2)
+                    .map(|w| (w[0], w[1]))
+                    .collect::<Vec<_>>();
+
+                windows.iter().enumerate().any(|(i, &(a, b))| {
+                    a == b
+                        && (i == 0 || windows[i - 1].0 != a)
+                        && (i == windows.len() - 1 || windows[i + 1].1 != a)
+                }) && windows.iter().all(|(a, b)| a <= b)
+            })
+            .count();
+
+        Ok(count)
+    }
+}
+
+fn main() {
+    aoc_lib::cli::<Day4>(PuzzleDate::new(2019, 4));
+}
